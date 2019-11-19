fddc27cf6bcb0372dd96ca86ec581eefe12e5397
Þórhallur Auður Helgason <thh114@hi.is>
2019-11-19 11:36:02 +0000

    bjó til auka hjálparföll í helpers.js og vann smá í list.js, ekki tilbúið fyrir fimmaura samt

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,27 @@
+// fjarlægir öll börn elements
 export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+// hjálparfall til að útbúa element
+// þarf að sníða að okkar þörfum, staðla fyrir
+// attributes sem við viljum vísa í og þannig, ekki búið
+// eflaust best að búa til tvö föll, eitt fyrir forsíðu
+// og annað fyrir fyrirlestrasíðu
+export function elList(type, className, clickHandler) {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  if (clickHandler)
+    element.addEventListener('click', clickHandler);
+  return element;
+}
+
+export function elLecture(type, className, clickHandler) {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  if (clickHandler)
+    element.addEventListener('click', clickHandler);
+  return element;
 }
--- src/lib/list.js
+++ src/lib/list.js
@@ -1,4 +1,4 @@
-import { empty } from './helpers';
+import { empty, elList, elLecture } from './helpers';
 
 export default class List {
   constructor() {
@@ -7,5 +7,21 @@ export default class List {
 
   load() {
     empty(this.container);
+    // þarf að skoða betur hvernig parse virkar, hvernig við
+    // vísum í hvert element í lectureCards og hvernig við
+    // búum síðan til element úr því. Mér datt í hug að við
+    // gætum bara byrjað á því að skrifa fyrir öll category
+    // og síðan hent argument inn í load fyrir hvern button,
+    // þ.e.a.s. load(css) væri ClickHandler fyrir CSS button
+    // o.s.frv. Ég veit ekki hvort það sem er að neðan er
+    // vitrænt... Þurfti bara að hlaupa áður en ég komst lengra.
+    lectureCards = JSON.parse("lectures.json");
+    let slugs = lectureCards.slug;
+    if(lectureCards.category = html) {
+      for(slug of slugs) {
+
+      }
+
+    }
   }
 }
