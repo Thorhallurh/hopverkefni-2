32cb005475e29968439fadf6f5e1f6f96d35a884
Þórhallur Auður Helgason <thh114@hi.is>
2019-11-28 14:51:12 +0000

    eslint lagað, allir linkar og slóðir á myndir etc. virka núna bæði með npm run dev nálgun og á netinu (ekki sjálfsagt, kemur í ljós)

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,12 +1,12 @@
-import { empty, el, clicked } from "./helpers";
-import { makeImage, makeTitle } from "./maker";
+import { empty, el, clicked } from './helpers';
+import { makeImage, makeTitle } from './maker';
 
 const filters = [];
 
 export default class List {
   constructor() {
-    this.container = document.querySelector(".cards");
-    this.jsonfile = "./lectures.json";
+    this.container = document.querySelector('.cards');
+    this.jsonfile = './lectures.json';
   }
 
   load() {
@@ -18,23 +18,23 @@ export default class List {
       }
     }
 
-    document.querySelector(".button__Html").addEventListener("click", e => {
+    document.querySelector('.button__Html').addEventListener('click', (e) => {
       clicked(e);
-      toggleFilter("html");
+      toggleFilter('html');
       this.fetchData();
     });
 
-    document.querySelector(".button__Css").addEventListener("click", e => {
+    document.querySelector('.button__Css').addEventListener('click', (e) => {
       clicked(e);
-      toggleFilter("css");
+      toggleFilter('css');
       this.fetchData();
     });
 
     document
-      .querySelector(".button__Javascript")
-      .addEventListener("click", e => {
+      .querySelector('.button__Javascript')
+      .addEventListener('click', (e) => {
         clicked(e);
-        toggleFilter("javascript");
+        toggleFilter('javascript');
         this.fetchData();
       });
 
@@ -46,32 +46,32 @@ export default class List {
    * Nær í gögn
    */
   fetchData() {
-    fetch("./lectures.json")
-      .then(response => {
+    fetch('./lectures.json')
+      .then((response) => {
         if (!response.ok) {
-          throw new Error("Gat ekki sótt fyrirlestra");
+          throw new Error('Gat ekki sótt fyrirlestra');
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         const lectures = data[Object.keys(data)[0]];
-        const filteredLectures = lectures.filter(value => {
+        const filteredLectures = lectures.filter((value) => {
           const chosenLect = filters.indexOf(value.category) !== -1;
           return chosenLect;
         });
         if (filters.length === 0) {
           empty(this.container);
-          lectures.forEach(item => {
+          lectures.forEach((item) => {
             this.getCard(item);
           });
         } else {
           empty(this.container);
-          filteredLectures.forEach(item => {
+          filteredLectures.forEach((item) => {
             this.getCard(item);
           });
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }
@@ -81,15 +81,15 @@ export default class List {
    */
   getCard(item) {
     // Div fyrir öll gögnin
-    const card = el("a");
+    const card = el('a');
     const addOnURL = `fyrirlestur.html?slug=${item.slug}`;
     const currentURL = window.location.href;
-    if (currentURL.includes("index.html")) {
-      card.href = currentURL.replace("index.html", addOnURL);
+    if (currentURL.includes('index.html')) {
+      card.href = currentURL.replace('index.html', addOnURL);
     } else {
       card.href = addOnURL;
     }
-    card.classList.add("card");
+    card.classList.add('card');
     this.container.appendChild(card);
 
     // Thumbnail fyrir fyrirlestur
@@ -97,28 +97,28 @@ export default class List {
     card.appendChild(img);
 
     // Div fyrir flokk og titil
-    const titlecatdiv = el("div");
-    titlecatdiv.classList.add("card__titlecat");
+    const titlecatdiv = el('div');
+    titlecatdiv.classList.add('card__titlecat');
     card.appendChild(titlecatdiv);
 
     // Div fyrir title og flokk í sitthvoru lagi
     // víxlaði til að setja div í rétta röð upp á útlit
-    const catdiv = el("div");
-    catdiv.classList.add("card__category");
+    const catdiv = el('div');
+    catdiv.classList.add('card__category');
     titlecatdiv.appendChild(catdiv);
-    const cat = el("h3", item.category);
-    cat.classList.add("card__h3");
+    const cat = el('h3', item.category);
+    cat.classList.add('card__h3');
     catdiv.appendChild(cat);
 
-    const titlediv = el("div");
-    titlediv.classList.add("card__title");
+    const titlediv = el('div');
+    titlediv.classList.add('card__title');
     titlecatdiv.appendChild(titlediv);
     const title = makeTitle(item.title, item.slug);
     titlediv.appendChild(title);
 
-    if (localStorage.getItem(item.slug) === "done") {
-      const checkMark = el("div", "✓");
-      checkMark.classList.add("card__finished");
+    if (localStorage.getItem(item.slug) === 'done') {
+      const checkMark = el('div', '✓');
+      checkMark.classList.add('card__finished');
       card.appendChild(checkMark);
     }
   }
